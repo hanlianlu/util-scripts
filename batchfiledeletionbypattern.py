bf1d1611c1660616ade86423d02c3e8e95fe91a6
Hanlian Lu <hanlianlu@gmail.com>
2021-12-17 00:01:27 +0100

    performance update

--- batchfiledeletionbypattern.py
+++ batchfiledeletionbypattern.py
@@ -4,13 +4,18 @@
 import os
 import pandas as pd
 import fnmatch
+from itertools import product
+from functools import reduce, partial
+
+from pandas.core.construction import is_empty_data
 
 ##To Configure: Batchfiles' root path, and Filterfile must be excel.
-dirPath = r"\\nasweucvot2001p.got.volvocars.net\C_9413_APP_NASsympathy01\data_srec_secret"
+#dirPath = r"\\nasweucvot2001p.got.volvocars.net\C_9413_APP_NASsympathy01\data_srec_secret"
+dirPath=r"C:\DevTest"
 dirFilterfile = r"\\nasweucvot2001p.got.volvocars.net\C_9413_APP_NASsympathy01\temp\table_data.xlsx"
 filterExcel_sheet_name = "CHINA CarID"
 
-def prepPattern(filterPath,excelsheet_name="Sheet1"):
+def prepLookup(filterPath,excelsheet_name="Sheet1"):
     try:
         df_ref = pd.read_excel(filterPath, sheet_name=excelsheet_name)
     except:
@@ -18,23 +23,17 @@ def prepPattern(filterPath,excelsheet_name="Sheet1"):
     ref_list=df_ref.iloc[:,0].tolist()
     return ref_list
 
-def removeFilesByMatchingPattern(dirPath, pattern):
-    #pattern: list
-    #dirPath: str
-    listOfFilesWithError = []
+def removeFilesbyMatchLookup(dirPath, lookuplist):
     for parentDir, dirnames, filenames in os.walk(dirPath):
-        for filename in filenames:
-            for patternelement in pattern:
-                if patternelement in filename:
-                    try:
-                        os.remove(os.path.join(parentDir, filename))
-                    except:
-                        #print("Error while deleting file : ", os.path.join(parentDir, filename))
-                        listOfFilesWithError.append(os.path.join(parentDir, filename))
-        print("Currently proceeding on :", str(parentDir),"\n")
-    return listOfFilesWithError
+        print("Current Dir is: ", str(parentDir))
+        [[matchFilenamebyLookup(lookupelement, filename, parentDir) for lookupelement in lookuplist] for filename in filenames]
 
-def removeFilesByMatchingPattern_naive(dirPath, pattern):
+def matchFilenamebyLookup(lookupelement, filename, path):
+    if lookupelement in filename:
+        os.remove(os.path.join(path,filename))
+        
+
+def removeFilesbyPattern(dirPath, pattern):
     #pattern: str, e.g. "TEST-*.xml" etc.
         # '*' matches everything; '?' matches any single character,
         # '[seq]' matches any character in seq; '[!seq]' matches any character not in seq.
@@ -52,15 +51,10 @@ def removeFilesByMatchingPattern_naive(dirPath, pattern):
 
 if __name__ == '__main__':
     print("Is dirFilter and file existed :",str(os.path.exists(dirFilterfile)) )
-    ref_list= prepPattern(dirFilterfile, excelsheet_name=filterExcel_sheet_name)
-    listOfErrors = removeFilesByMatchingPattern(dirPath, ref_list)
+    ref_list= prepLookup(dirFilterfile, excelsheet_name=filterExcel_sheet_name)
+    removeFilesbyMatchLookup(dirPath, ref_list)
+    print("Batch deletion completed :)") 
+
 
     #ref_pattern = "Test_*.sydata"
-    #listOfErrors = removeFilesByMatchingPattern_naive(dirPath, ref_pattern)
-
-    if len(listOfErrors)>1:
-        print('Files that can not be deleted : ')
-        for filePath in listOfErrors:
-            print(filePath)
-    else:
-        print("Batch deletion completed with no exception :) ")
+    #listOfErrors = removeFilesbyPattern(dirPath, ref_pattern)
